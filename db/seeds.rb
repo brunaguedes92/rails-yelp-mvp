7b45fa1c96c679037cf6848c52cec58e846f8a21
Bruna Guedes <bruna.guedes02@gmail.com>
2020-11-27 15:57:24 -0300

    cruds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning the db...'
+
+Restaurant.destroy_all
+
+puts 'db is clean'
+puts 'creating restaurants...'
+
+5.times do
+  restaurant = Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.phone_number_with_country_code,
+    category: %w[chinese italian japanese french belgian].sample
+  )
+  rand(1..5).times do
+    review = Review.create(
+      rating: rand(0..5),
+      content: Faker::Restaurant.review,
+      restaurant: restaurant
+    )
+    puts "#{restaurant.id}-#{restaurant.name}'s review was created"
+  end
+  puts "#{restaurant.id}-#{restaurant.name} was created"
+end
